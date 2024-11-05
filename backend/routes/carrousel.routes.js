9a66627899d64f947c44541a5ad711638c5c2d71
Brent <116728958+BrentCaesPXL@users.noreply.github.com>
2024-11-05 11:43:58 +0100

    Update carrousel.routes.js

--- backend/routes/carrousel.routes.js
+++ backend/routes/carrousel.routes.js
@@ -4,11 +4,12 @@ const carrouselRouter = express.Router();
 
 // Configureer de AWS SDK
 const s3 = new AWS.S3({
-  region: 'us-west-2', // Pas aan naar de regio van je bucket
+  region: 'us-east-1', // Pas aan naar de regio van je bucket
 });
 
 carrouselRouter.get('', async (req, res) => {
   const bucketName = 'my-very-special-bucket-brentcaes';
+  const region = 'us-east-1';
 
   try {
     // Vraag de lijst van objecten in de bucket op
@@ -19,7 +20,7 @@ carrouselRouter.get('', async (req, res) => {
     const data = await s3.listObjectsV2(params).promise();
     
     // Maak een lijst met de URLs van alle afbeeldingsbestanden
-    const imageUrls = data.Contents.map(file => `https://${bucketName}.s3.amazonaws.com/${file.Key}`);
+    const imageUrls = data.Contents.map(file => `https://${bucketName}.s3.${region}.amazonaws.com/${file.Key}`);
     
     // Stuur de lijst met afbeeldings-URLs als JSON terug
     res.json(imageUrls);
@@ -31,4 +32,3 @@ carrouselRouter.get('', async (req, res) => {
 });
 
 module.exports = carrouselRouter;
-
